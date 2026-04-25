ef26b4097247d773c2f09e6c3d071e9a8d448729
tik8877 <tik88755@gmail.com>
2026-04-25 15:07:59 +0000

    task 1

--- bash/1.sh
+++ bash/1.sh
@@ -1,2 +1,13 @@
 #!/bin/bash
 
+file=$1
+time=$2
+
+for ((i=0; i<time; i++))
+do
+    load=$(cat /proc/loadavg)
+    date_str=$(date "+%d.%m.%y %H:%M")
+    echo "$date_str = $load" >> "$file"
+    sleep 1
+done
+
