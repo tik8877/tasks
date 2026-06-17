a3949c9d6cb4a44829290057ebdc60f5ff6eeace
tik8877 <tik88755@gmail.com>
2026-06-17 12:04:44 +0000

    проверка ci/cd

--- docker/src/models.py
+++ docker/src/models.py
@@ -1,3 +1,4 @@
+from sqlalchemy import String
 from sqlalchemy.orm import Mapped, mapped_column
 
 from src.database import Base
@@ -6,5 +7,5 @@ from src.database import Base
 class User(Base):
     __tablename__ = "users"
 
-    id: Mapped[int] = mapped_column(primary_key=True, auto_increment=True)
-    name: Mapped[str]
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[str] = mapped_column(String(255), nullable=False)
